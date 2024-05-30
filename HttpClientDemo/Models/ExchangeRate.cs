eeaf12a52a06989cdbbba19a841fb95426b38c27
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-05-30 13:06:11 +0200

    Use file-scoped namespaces

--- HttpClientDemo/Models/ExchangeRate.cs
+++ HttpClientDemo/Models/ExchangeRate.cs
@@ -1,23 +1,21 @@
 ﻿using System.Text.Json.Serialization;
 
-namespace HttpClientDemo.Models
+namespace HttpClientDemo.Models;
+
+public class ExchangeRate
 {
+    [JsonPropertyName("country")]
+    public string Country { get; set; }
 
-    public class ExchangeRate
-    {
-        [JsonPropertyName("country")]
-        public string Country { get; set; }
+    [JsonPropertyName("currency")]
+    public string Currency { get; set; }
 
-        [JsonPropertyName("currency")]
-        public string Currency { get; set; }
+    [JsonPropertyName("amount")]
+    public int Amount { get; set; }
 
-        [JsonPropertyName("amount")]
-        public int Amount { get; set; }
+    [JsonPropertyName("rate")]
+    public decimal Rate { get; set; }
 
-        [JsonPropertyName("rate")]
-        public decimal Rate { get; set; }
-
-        [JsonPropertyName("currencyCode")]
-        public string CurrencyCode { get; set; }
-    }
+    [JsonPropertyName("currencyCode")]
+    public string CurrencyCode { get; set; }
 }
--- HttpClientDemo/Startup.cs
+++ HttpClientDemo/Startup.cs
@@ -7,56 +7,55 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace HttpClientDemo
+namespace HttpClientDemo;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddRazorPages();
+        services.AddServerSideBlazor();
+        services.AddScoped<ScrapingService>();
+        services.AddScoped<ExchangeRateService>();
+        // Register HttpClient as a transient
+        services.AddScoped<HttpClient>();
+        services.AddHttpClient("ExchangeRateClient", client =>
         {
-            Configuration = configuration;
+            client.BaseAddress = new Uri("https://api.cnb.cz/");
+        });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
 
-        public IConfiguration Configuration { get; }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
         {
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddScoped<ScrapingService>();
-            services.AddScoped<ExchangeRateService>();
-            // Register HttpClient as a transient
-            services.AddScoped<HttpClient>();
-            services.AddHttpClient("ExchangeRateClient", client =>
-            {
-                client.BaseAddress = new Uri("https://api.cnb.cz/");
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapBlazorHub();
-                endpoints.MapFallbackToPage("/_Host");
-            });
-        }
+            endpoints.MapBlazorHub();
+            endpoints.MapFallbackToPage("/_Host");
+        });
     }
 }
