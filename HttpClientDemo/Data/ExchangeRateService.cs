047fa49c94cac07aadd429e2cf4aa09a53e7106b
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-05-30 13:06:11 +0200

    Use file-scoped namespaces

--- HttpClientDemo/Data/ExchangeRateService.cs
+++ HttpClientDemo/Data/ExchangeRateService.cs
@@ -5,43 +5,42 @@ using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using HttpClientDemo.Models;
 
-namespace HttpClientDemo.Data
+namespace HttpClientDemo.Data;
+
+public class ExchangeRateService
 {
-    public class ExchangeRateService
+    private readonly HttpClient _httpClient;
+    private const string Endpoint = "cnbapi/exrates/daily";
+
+    public ExchangeRateService(IHttpClientFactory httpClientFactory)
     {
-        private readonly HttpClient _httpClient;
-        private const string Endpoint = "cnbapi/exrates/daily";
+        _httpClient = httpClientFactory.CreateClient("ExchangeRateClient");
+    }
 
-        public ExchangeRateService(IHttpClientFactory httpClientFactory)
+    public async Task<List<ExchangeRate>> GetExchangeRatesAsync()
+    {
+        var response = await _httpClient.GetStringAsync(Endpoint);
+        var data = JsonSerializer.Deserialize<ExchangeRateResponse>(response);
+        var rates = data?.Rates;
+
+        if (rates != null)
         {
-            _httpClient = httpClientFactory.CreateClient("ExchangeRateClient");
-        }
-
-        public async Task<List<ExchangeRate>> GetExchangeRatesAsync()
-        {
-            var response = await _httpClient.GetStringAsync(Endpoint);
-            var data = JsonSerializer.Deserialize<ExchangeRateResponse>(response);
-            var rates = data?.Rates;
-
-            if (rates != null)
+            foreach (var rate in rates)
             {
-                foreach (var rate in rates)
+                if (rate.Amount != 1)
                 {
-                    if (rate.Amount != 1)
-                    {
-                        rate.Rate /= rate.Amount;
-                        rate.Amount = 1;
-                    }
+                    rate.Rate /= rate.Amount;
+                    rate.Amount = 1;
                 }
             }
-
-            return rates;
         }
-    }
 
-    public class ExchangeRateResponse
-    {
-        [JsonPropertyName("rates")]
-        public List<ExchangeRate> Rates { get; set; }
+        return rates;
     }
+}
+
+public class ExchangeRateResponse
+{
+    [JsonPropertyName("rates")]
+    public List<ExchangeRate> Rates { get; set; }
 }
